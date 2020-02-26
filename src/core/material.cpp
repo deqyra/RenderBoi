79020796eed1b374d80b22d76b2dc19502ac8c9f
deqyra <deqyra@gmail.com>
2020-02-26 09:18:07 +0100

    Upgrade Material to handle multiple diffuse and specular maps

--- src/core/material.cpp
+++ src/core/material.cpp
@@ -1,19 +1,18 @@
 #include "material.hpp"
 
 Material::Material() :
-    ambient(glm::vec3(0.15f)),
-    diffuse(glm::vec3(0.6f)),
-    specular(glm::vec3(1.f)),
-    shininess(12.8f)
+    Material(glm::vec3(0.15f), glm::vec3(0.6f), glm::vec3(1.f), 12.8f)
 {
 
 }
 
-Material::Material(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float shininess) :
+Material::Material(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float shininess, std::vector<Texture2D> diffuseMaps, std::vector<Texture2D> specularMaps) :
     ambient(ambient),
     diffuse(diffuse),
     specular(specular),
-    shininess(shininess)
+    shininess(shininess),
+    diffuseMaps(diffuseMaps),
+    specularMaps(specularMaps)
 {
 
 }
--- src/core/material.hpp
+++ src/core/material.hpp
@@ -2,17 +2,32 @@
 #define MATERIAL_HPP
 
 #include <glm/glm.hpp>
+#include <vector>
+
+#include "texture_2d.hpp"
+
+#define DIFFUSE_MAP_MAX_COUNT   8
+#define SPECULAR_MAP_MAX_COUNT  8
 
 class Material
 {
     public:
         Material();
-        Material(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float shininess);
+        Material(glm::vec3 ambient,
+                 glm::vec3 diffuse,
+                 glm::vec3 specular,
+                 float shininess,
+                 std::vector<Texture2D> diffuseMaps = std::vector<Texture2D>(),
+                 std::vector<Texture2D> specularMaps = std::vector<Texture2D>()
+        );
 
         glm::vec3 ambient;
         glm::vec3 diffuse;
         glm::vec3 specular;
         float shininess;
+
+        std::vector<Texture2D> diffuseMaps;
+        std::vector<Texture2D> specularMaps;
 };
 
 #endif//MATERIAL_HPP
