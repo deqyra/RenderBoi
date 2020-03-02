8316f1c36c53bd559ab6a17430d2fc429c35e5ac
deqyra <deqyra@gmail.com>
2020-03-02 10:13:36 +0100

    Write Scene class

---
+++ src/core/scene.cpp
@@ -0,0 +1,200 @@
+#include "scene.hpp"
+
+Scene::Scene() :
+    _graph(std::make_shared<SceneObject>()),
+    _modelMatrices(glm::mat4(1.f)),
+    _objectIdsToNodeIds()
+{
+    auto rootNode = _graph.getRoot().lock();
+    auto matRootNode = _modelMatrices.getRoot().lock();
+    unsigned int rootObjId = rootNode->value->id;
+    _objectIdsToNodeIds[rootObjId] = { rootNode->id, matRootNode->id };
+}
+
+Scene::WeakObjPtr Scene::operator[](unsigned int id)
+{
+    auto it = _objectIdsToNodeIds.find(id);
+    if (it == _objectIdsToNodeIds.end())
+    {
+        std::string s = "Scene: no SceneObject with ID " + std::to_string(id) + ".";
+        throw std::runtime_error(s.c_str());
+    }
+
+    auto pair = it->second;
+    auto node = _graph[pair.first].lock();
+    return node->value;
+}
+
+void Scene::registerObject(ObjPtr object, unsigned int parentId)
+{
+    auto it = _objectIdsToNodeIds.find(parentId);
+    if (it == _objectIdsToNodeIds.end())
+    {
+        std::string s = "Scene: no SceneObject with ID " + std::to_string(parentId) + ", cannot register new object as one of its children.";
+        throw std::runtime_error(s.c_str());
+    }
+
+    auto pair = it->second;
+    unsigned int graphNodeId = _graph.addNode(object, pair.first);
+    unsigned int matrixNodeId = _modelMatrices.addNode(glm::mat4(1.f), pair.second);
+
+    _objectIdsToNodeIds[object->id] = { graphNodeId, matrixNodeId };
+    recalculateModelMatrix(object->id);
+}
+
+void Scene::registerObject(ObjPtr object)
+{
+    auto node = _graph.getRoot().lock();
+    registerObject(object, node->value->id);
+}
+
+void Scene::removeObject(unsigned int id)
+{
+    auto it = _objectIdsToNodeIds.find(id);
+    if (it == _objectIdsToNodeIds.end())
+    {
+        std::string s = "Scene: no SceneObject with ID " + std::to_string(id) + ", cannot remove object.";
+        throw std::runtime_error(s.c_str());
+    }
+
+    auto pair = it->second;
+    _graph.removeBranch(pair.first);
+    _modelMatrices.removeBranch(pair.second);
+
+    removeIdMapping(id);
+}
+
+void Scene::moveObject(unsigned int id, unsigned int newParentId)
+{
+    auto objectIt = _objectIdsToNodeIds.find(id);
+    if (objectIt == _objectIdsToNodeIds.end())
+    {
+        std::string s = "Scene: no SceneObject with ID " + std::to_string(id) + ", cannot move object.";
+        throw std::runtime_error(s.c_str());
+    }
+
+    auto parentIt = _objectIdsToNodeIds.find(newParentId);
+    if (parentIt == _objectIdsToNodeIds.end())
+    {
+        std::string s = "Scene: no SceneObject with ID " + std::to_string(newParentId) + ", cannot move object " + std::to_string(id) + " to it.";
+        throw std::runtime_error(s.c_str());
+    }
+
+    auto objectPair = objectIt->second;
+    auto parentPair = parentIt->second;
+
+    _graph.moveBranch(objectPair.first, parentPair.first);
+    _modelMatrices.moveBranch(objectPair.second, parentPair.second);
+    recalculateModelMatrix(id);
+}
+
+glm::mat4 Scene::getWorldModelMatrix(unsigned int id)
+{
+    auto it = _objectIdsToNodeIds.find(id);
+    if (it == _objectIdsToNodeIds.end())
+    {
+        std::string s = "Scene: no SceneObject with ID " + std::to_string(id) + ", cannot retrieve model matrix.";
+        throw std::runtime_error(s.c_str());
+    }
+
+    processOutdatedTransformsFromNode(id);
+
+    auto pair = it->second;
+    auto node = _modelMatrices[pair.second].lock();
+    return node->value;
+}
+
+std::vector<Scene::WeakObjPtr> Scene::getAllObjects()
+{
+    std::vector<WeakObjPtr> result;
+
+    for (auto it = _objectIdsToNodeIds.begin(); it != _objectIdsToNodeIds.end(); it++)
+    {
+        auto pair = it->second;
+        auto node = _graph[pair.first].lock();
+        result.push_back(node->value);
+    }
+
+    return result;
+}
+
+void Scene::processOutdatedTransformsFromNode(unsigned int id)
+{
+    auto it = _objectIdsToNodeIds.find(id);
+    auto pair = it->second;
+
+    ObjTree::NodePtr objNode = _graph[pair.first].lock();
+    std::vector<ObjTree::WeakNodePtr> parentChain = objNode->getParentChain();
+
+    bool outdated = false;
+    unsigned int uppermostOutdatedId;
+    for (auto it = parentChain.begin(); it != parentChain.end(); it++)
+    {
+        ObjPtr obj = it->lock()->value;
+        if (obj->transformModifiedFlagState())
+        {
+            obj->resetTransformModifiedFlag();
+            uppermostOutdatedId = obj->id;
+            outdated = true;
+        }
+    }
+
+    if (outdated)
+    {
+        recalculateModelMatrix(uppermostOutdatedId);
+    }
+}
+
+void Scene::recalculateModelMatrix(unsigned int id)
+{
+    auto it = _objectIdsToNodeIds.find(id);
+    auto pair = it->second;
+
+    ObjTree::NodePtr objNode = _graph[pair.first].lock();
+    MatTree::NodePtr matNode = _modelMatrices[pair.second].lock();
+    MatTree::NodePtr parentMatNode = matNode->getParent().lock();
+
+    glm::mat4 localTransform = objNode->value->getModelMatrix();
+    glm::mat4 worldTransform = glm::mat4(1.f);
+    if (parentMatNode.get() != nullptr) worldTransform = parentMatNode->value;
+
+    matNode->value = worldTransform * localTransform;
+
+    auto children = objNode->getChildren();
+    for (auto childIt = children.begin(); childIt != children.end(); childIt++)
+    {
+        auto strongChild = childIt->lock();
+        recalculateModelMatrix(strongChild->id);
+    }
+}
+
+void Scene::removeIdMapping(unsigned int id)
+{
+    auto it = _objectIdsToNodeIds.find(id);
+    auto pair = it->second;
+
+    ObjTree::NodePtr objNode = _graph[pair.first].lock();
+    auto children = objNode->getChildren();
+    for (auto childIt = children.begin(); childIt != children.end(); childIt++)
+    {
+        auto strongChild = childIt->lock();
+        removeIdMapping(strongChild->id);
+    }
+
+    _objectIdsToNodeIds.erase(id);
+}
+
+bool Scene::hasDisabledParent(unsigned int id)
+{
+    auto it = _objectIdsToNodeIds.find(id);
+    auto pair = it->second;
+
+    ObjTree::NodePtr objNode = _graph[pair.first].lock();
+    std::vector<ObjTree::WeakNodePtr> parents = objNode->getParentChain();
+    for (auto it = parents.begin(); it != parents.end(); it++)
+    {
+        auto strongParent = it->lock();
+        if (!strongParent->value->enabled) return true;
+    }
+    return false;
+}
---
+++ src/core/scene.hpp
@@ -0,0 +1,66 @@
+#ifndef SCENE_HPP
+#define SCENE_HPP
+
+#include <algorithm>
+#include <functional>
+#include <unordered_map>
+#include <vector>
+#include <glm/glm.hpp>
+
+#include "../tools/tree.hpp"
+#include "scene/scene_object.hpp"
+
+class Scene
+{
+    public:
+        using ObjPtr = std::shared_ptr<SceneObject>;
+        using WeakObjPtr = std::weak_ptr<SceneObject>;
+        using ObjTree = Tree<ObjPtr>;
+        using MatTree = Tree<glm::mat4>;
+
+    private:
+        ObjTree _graph;
+        MatTree _modelMatrices;
+        std::unordered_map<unsigned int, std::pair<unsigned int, unsigned int>> _objectIdsToNodeIds;
+
+        void processOutdatedTransformsFromNode(unsigned int id);
+        void recalculateModelMatrix(unsigned int id);
+        void removeIdMapping(unsigned int id);
+        bool hasDisabledParent(unsigned int id);
+
+    public:
+        Scene();
+
+        WeakObjPtr operator[](unsigned int id);
+        void registerObject(ObjPtr object, unsigned int parentId);
+        void registerObject(ObjPtr object);
+        void removeObject(unsigned int id);
+        void moveObject(unsigned int id, unsigned int newParentId);
+        glm::mat4 getWorldModelMatrix(unsigned int id);
+
+        std::vector<WeakObjPtr> getAllObjects();
+
+        template<class T>
+        std::vector<WeakObjPtr> getObjectsWithComponent(bool mustBeEnabled = true);
+};
+
+template<class T>
+std::vector<Scene::WeakObjPtr> Scene::getObjectsWithComponent(bool mustBeEnabled)
+{
+    std::vector<WeakObjPtr> all = getAllObjects();
+    std::vector<WeakObjPtr> result;
+
+    std::function<bool(WeakObjPtr)> componentChecker = [this, mustBeEnabled](WeakObjPtr obj)
+    {
+        auto strongObj = obj.lock();
+        if (mustBeEnabled && !strongObj->enabled) return false;
+        if (mustBeEnabled && hasDisabledParent(strongObj->id)) return false;
+        return strongObj->hasComponent<T>();
+    };
+
+    std::copy_if(all.begin(), all.end(), std::back_inserter(result), componentChecker);
+
+    return result;
+}
+
+#endif//SCENE_HPP
