88e62386b70a6515327cd90e43870706862799f7
deqyra <deqyra@gmail.com>
2020-03-05 16:09:37 +0100

    Add attenuation data to PointLight

--- src/core/lights/point_light.cpp
+++ src/core/lights/point_light.cpp
@@ -1,19 +1,46 @@
 #include "point_light.hpp"
 
+const std::function<float(float)> PointLight::linearCoeffFromDesiredRange = [](float range) -> float
+{
+    return (2.041118599e-7 * range * range) - (7.382441708e-4 * range) + 2.481753022e-1;
+};
+
+const std::function<float(float)> PointLight::quadraticCoeffFromDesiredRange = [](float range) -> float
+{
+    return (3.682978425e-7 * range * range) - (1.318877109e-3 * range) + 4.032644093e-1;
+};
+
 PointLight::PointLight() :
-    Light(LightType::PointLight),
-    ambient(glm::vec3(1.f)),
-    diffuse(glm::vec3(1.f)),
-    specular(glm::vec3(1.f))
+    PointLight(50.f)
 {
 
 }
 
-PointLight::PointLight(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular) :
+PointLight::PointLight(float range) :
+    PointLight(glm::vec3(1.f), glm::vec3(1.f), glm::vec3(1.f), range)
+{
+
+}
+
+PointLight::PointLight(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float range) :
     Light(LightType::PointLight),
     ambient(ambient),
     diffuse(diffuse),
     specular(specular)
+{
+    constant = 1.f;
+    linear = linearCoeffFromDesiredRange(range);
+    quadratic = quadraticCoeffFromDesiredRange(range);
+}
+
+PointLight::PointLight(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic) :
+    Light(LightType::PointLight),
+    ambient(ambient),
+    diffuse(diffuse),
+    specular(specular),
+    constant(constant),
+    linear(linear),
+    quadratic(quadratic)
 {
 
 }
--- src/core/lights/point_light.hpp
+++ src/core/lights/point_light.hpp
@@ -1,24 +1,33 @@
 #ifndef POINT_LIGHT_HPP
 #define POINT_LIGHT_HPP
 
+#include <functional>
 #include <glm/glm.hpp>
 
-#include "../positioned_object.hpp"
 #include "../light.hpp"
 #include "../light_type.hpp"
 
-#define POINT_LIGHT_MAX_COUNT 128
+#define POINT_LIGHT_MAX_COUNT 64
 #define POINT_LIGHT_UBO_SIZE  64
 
 class PointLight : public Light
 {
     public:
         PointLight();
-        PointLight(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular);
+        PointLight(float range);
+        PointLight(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float range);
+        PointLight(glm::vec3 ambient, glm::vec3 diffuse, glm::vec3 specular, float constant, float linear, float quadratic);
 
         glm::vec3 ambient;
         glm::vec3 diffuse;
         glm::vec3 specular;
+
+        float constant;
+        float linear;
+        float quadratic;
+
+        static const std::function<float(float)> linearCoeffFromDesiredRange;
+        static const std::function<float(float)> quadraticCoeffFromDesiredRange;
 };
 
 #endif//POINT_LIGHT_HPP
