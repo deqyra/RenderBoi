c2865cf34d6cf77ece6ada074c74aebb7637e517
deqyra <deqyra@gmail.com>
2020-10-13 13:48:02 +0200

    Add all-in-one scene+mesh creation method to factory

--- core/factory.hpp
+++ core/factory.hpp
@@ -11,20 +11,39 @@
 class Factory
 {
     public:
-        /// Returns an initialized scene
+        /// @brief Instantiate and initiliaze a new scene.
+        ///
+        /// @return Pointer to the instantiated scene.
         static ScenePtr makeScene();
 
-        /// Returns an initialized scene object
+        /// @brief Instantiate and initialize a new scene object.
+        ///
+        /// @return Pointer to the instantiated scene object.
         static SceneObjectPtr makeSceneObject();
 
-        /// Returns a mesh whose vertices were generated according to parameters
+        /// @brief Create a mesh with vertices arranged in a certain shape.
         ///
-        /// @tparam T Litteral describing which type of mesh to generate
-        /// @tparam ArgTypes Types of arguments to pass to the chosen mesh generator
+        /// @tparam T Litteral describing which mesh generator to use.
         ///
-        /// @param[in] args Parameters of the mesh generation
+        /// @param[in] parameters Parameters to pass to the mesh generator.
+        /// Look up generators in core/mesh_generators/ for details.
+        ///
+        /// @return Pointer to the generated mesh.
         template<MeshType T>
         static MeshPtr makeMesh(typename TypeToGenMapping<T>::GenType::Parameters parameters);
+
+        /// @brief Instantiate and initiliaze a scene object, generate a mesh,
+        /// and attach it to the scene object within a mesh component.
+        ///
+        /// @tparam T Litteral describing which mesh generator to use.
+        ///
+        /// @param[in] parameters Parameters of the mesh generation.
+        /// @param[in] mat Material to render the mesh in.
+        /// @param[in] shader Shader to render the mesh with.
+        ///
+        /// @return Pointer to the instantiated scene object.
+        template<MeshType T>
+        static SceneObjectPtr makeSceneObjectWithMesh(typename TypeToGenMapping<T>::GenType::Parameters parameters, Material mat = Material(), Shader shader = Shader());
 };
 
 template<MeshType T>
@@ -33,6 +52,15 @@ MeshPtr Factory::makeMesh(typename TypeToGenMapping<T>::GenType::Parameters para
     using GenType = typename TypeToGenMapping<T>::GenType;
     std::shared_ptr<GenType> gen = std::make_shared<GenType>(parameters);
     return gen->generatePtr();
+}
+
+template<MeshType T>
+SceneObjectPtr Factory::makeSceneObjectWithMesh(typename TypeToGenMapping<T>::GenType::Parameters parameters, Material mat, Shader shader)
+{
+    MeshPtr mesh = Factory::makeMesh<T>(parameters);
+    SceneObjectPtr obj = Factory::makeSceneObject();
+    obj->addComponent<MeshComponent>(mesh, mat, shader);
+    return obj;
 }
 
 #endif//CORE__FACTORY_HPP
